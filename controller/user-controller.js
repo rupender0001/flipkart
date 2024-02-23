da2c1cb0102a49596832901c8407594f0bd2a7e2
Kanishk Pareek <knshkpareek@gmail.com>
2024-02-23 19:21:28 +0530

    change

---
+++ controller/user-controller.js
@@ -0,0 +1,36 @@
+import User from '../model/userSchema.js';
+
+export const userLogIn = async (request, response) => {
+    try {
+        console.log(request.body)
+        let user = await User.findOne({ phone: request.body.phone, password: request.body.password });
+        console.log(`>>>>>>>>>>>`,user)
+        if(user) {
+            return response.status(200).json(`${user.username} login successfull`);
+        } else {
+            return response.status(401).json('Invalid Login');
+        }
+
+    } catch (error) {
+        response.json('Error: ', error.message);        
+    }
+}
+
+export const userSignUp = async (request, response) => {
+    try {
+        const exist = await User.findOne({ phone: request.body.phone });
+        if(exist) {
+            return response.status(401).json({ message: 'User already exist'});
+        }
+        const user = request.body;
+        const newUser = new User(user);
+        await newUser.save();
+        response.status(200).json({ message: user });
+        
+    } catch (error) {
+        response.status(500).json({ message: error.message });
+    }
+}
+
+
+
---
+++ routes/route.js
@@ -0,0 +1,21 @@
+import express from  'express';
+import { getProductById, getProducts } from '../controller/product-controller.js';
+import { userSignUp, userLogIn } from '../controller/user-controller.js';
+// import { addItemInCart } from '../controller/cart-controller.js';
+import {addMoney,verifyPay } from '../controller/payment-controller.js';
+
+const router = express.Router();
+
+//login & signup
+router.post('/signup', userSignUp);
+router.post('/login', userLogIn);
+
+router.get('/products', getProducts);
+router.get('/product/:id', getProductById);
+
+// router.post('/cart/add', addItemInCart);
+router.post('/orders',addMoney)
+router.post('/verify',verifyPay)
+
+
+export default router;
