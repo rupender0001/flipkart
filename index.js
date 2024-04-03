24212217d1a5e10bd02cf275dbb8e56e64796a66
Kanishk Pareek <knshkpareek@gmail.com>
2024-04-03 18:12:40 +0530

    changes

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ dotenv.config();
 const app = express();
 
 const PORT = 8000;
-import path from "path"
-const _dirname=path.dirname("")
-const buildPath=path.join(_dirname,"../frontend/build")
-app.use(express.static(buildPath));
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 console.log(`>>>>>>>>>`)
