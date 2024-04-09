6429aaab54b787a211d26d25ca50c9f3aea27bd1
Kanishk Pareek <knshkpareek@gmail.com>
2024-04-09 16:27:46 +0530

    changes

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ console.log(`>>>>>>>>>`)
 Connection(1234, 1234);
 console.log(`>>>>>>>>>>>>>sss>>>>`)
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
-// DefaultData();
+// DefaultData(); 
+
 
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
